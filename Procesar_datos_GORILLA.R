0210b1edd8453d273465848cd8b07d438fede231
Julieta <julilaurino@gmail.com>
2020-06-26 17:17:21 -0300

    final

--- Procesar_datos_GORILLA.R
+++ Procesar_datos_GORILLA.R
@@ -1,68 +1,101 @@
-#Instalo paquetes 
+# Parte 1 -----------------------------------------------------------------
+
+## **Configuraciones iniciales**  
+  
+#### **Paquetes**
 install.packages("tidyverse")
 install.packages("xlsx")
 
-# Setear el directorio de trabajo correspondiente
+#### **Directorio de trabajo**
 setwd("C:/Users/User/Desktop/Taller - Procesamiento de datos")
 
-#Abro los archivos
+## **Paso 1: Leo y almaceno la base de datos** 
+library(tidyverse)
 data_v14 <- read.csv("data_exp_11854-v14_task-5ksx.csv")
 data_v15 <- read.csv("data_exp_11854-v15_task-5ksx.csv")
 
-#Junto las dos bases de datos, una abajo de la otra 
+## **Paso 2: Unir bases de datos**
 data_raw <- bind_rows(data_v14, data_v15)
 
-#Uso rename para cambiar el nombre de algunas variables (columnas)
+## **Paso 3: Renombro columas**
 data_raw <- rename(data_raw,"id" = Participant.Public.ID, "group" = randomiser.qg34, "RT" = Reaction.Time, "Acc" = Correct, "relation" = primeType, "familiarity" = trialType, "block" = 	block_testingSR) 
 
-#Uso filter para quedarme con la Accuracy y RT del target. Además me quedo con la tarea (no analizo el demo) 
+## **Paso 4: Filtro filas** 
 data_raw <- filter (data_raw, Screen.Name == "target", display == "Tarea") 
 
-#Uso select para quedarme sólo con las columnas que me interesan
+
+## **Paso 5: Selecciono columnas**
 data_raw <- select (data_raw, id, group, relation, familiarity, block, prime, target, Response, Acc, RT) 
-  
-#Uso arrange para ordenar las filas 
+
+## **Bonus track I: Ordeno filas**
 data_raw <- arrange (data_raw, group, id, familiarity, block, target)
 
-#Todo junto!
+## **Bonus track II: Nuevas columnas** 
+data_raw <- mutate(data_raw, accRT = ifelse(Acc == 1, RT, NA))
+
+slice(data_raw, 1:10)
+
+## **Resumen y conlusión**
+### **Todo junto!**
 data_raw <- bind_rows(data_v14, data_v15) %>% 
   rename("id" = Participant.Public.ID, "group" = randomiser.qg34, "RT" = Reaction.Time, "Acc" = Correct, "relation" = primeType, "familiarity" = trialType, "block" = 	block_testingSR) %>% 
   filter (Screen.Name == "target", display == "Tarea") %>% 
   select (id, group, relation, familiarity, block, prime, target, Response, Acc, RT) %>% 
-  arrange (group, id, familiarity, block, target)
+  arrange (group, id, familiarity, block, target) %>% 
+  mutate (accRT = ifelse(Acc == 1, RT, NA))
 
-#Chequeo
+### **Chequeo**
 trials_count <- data_raw %>% 
   group_by(id) %>% 
   count()
 
-#Participantes outliers por accuracy
-data_outliers <- data_raw %>%
+slice(trials_count, 1:10)
+
+
+
+# Parte 2 -----------------------------------------------------------------
+
+## **Criterio de inclusión por accuracy**
+data_exclude <- data_raw %>%
   filter (familiarity == "familiar") %>% 
   group_by(id, group) %>% 
   summarise(meanAcc = mean(Acc)) %>% 
   filter(meanAcc < 0.9)
 
-#Los elimino de mi base de datos
-data <- data_raw %>% 
-  filter(!(id %in% data_outliers$id) & !(RT < 200))  
+slice(data_exclude, 1:20)
 
-#Análisis exploratorio
+data <- data_raw %>% 
+  filter(!(id %in% data_exclude$id) & !(RT < 200))
+
+## **Análisis exploratorio**
 data_condition <- data %>% 
   group_by(group, familiarity, relation) %>% 
   summarise(meanAcc = mean(Acc), 
             meanRT = mean(RT, na.rm = TRUE),
             sd_RT = sd(RT))
 
-#Para analizar RTs: uso mutate para generar una nueva columna que tenga sólo los RTs en los que la rta fue correcta. 
-data <- data %>% 
-  mutate(accRT = ifelse(Acc == 1, RT, NA))
+slice(data_condition, 1:8)
 
-#Cambio el tipo de base de datos (de larga a ancha) con pivot_wider 
+## **Guardado**
+library(xlsx)
+write.xlsx(data, file= "PRIMINGdata.gorilla.xlsx")
+
+## **Bonus track: cambio de tipo de base de datos**
+# Pivot wider
 data_wide <- data %>% 
   select(id, Acc, target, prime, relation) %>% 
   pivot_wider(names_from = id, values_from = Acc)
 
-#Vuelvo a la forma anterior con pivot_longer
+slice(data_wide, 1:10)
+
+# Pivot longer
 data_long <- data_wide %>% 
   pivot_longer(cols = c(-target, -prime, -relation), names_to = "id", values_to = "Acc")
+
+
+
+
+
+
+
+# FIN :)
